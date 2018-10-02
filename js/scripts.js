6cce3d5f85b316b043bac2d916b2c049f9287be7
Michael Butcher and Richard Laws <michael@>
2018-10-01 19:50:06 -0700

    Get JS logic to work for identifying palindrome single words and provide user the true or false results

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,24 @@ $(document).ready(function() {
     var userInput = $("input#words").val();
     var ifPalindrome = userInput.split("");
 
-    console.log(ifPalindrome);
-    // userInput
+    // var ifPalindrome2 = ifPalindrome.map(results.reverse());
+    var ifPalindrome2 = ifPalindrome.map(function(number) {
+  return number;
+});
+
+
+    ifPalindrome2.reverse();
+    // ifPalindrome.toString();
+    // ifPalindrome2.toString();
+
+
+
+    if (ifPalindrome.toString() === ifPalindrome2.toString()) {
+      alert("that's a palindrome!!");
+    }
+    else {
+      alert("that's NOT a palindrome!!");
+    };
+
     });
-  });
+});
